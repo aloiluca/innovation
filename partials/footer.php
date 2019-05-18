daa61afd32d203115e0679cc436e9c6267e48e32
Luca Aloi <luca.aloi@bitia.it>
2019-05-18 19:07:01 +0200

    aggiunto footer

---
+++ partials/footer.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<div class="footer">
+    <div class="footerFloat">
+        <img class="logo" src="resources/img/logo.png">
+        <a href=""><img class="instagram" src="resources/img/instagram.jpg"></a>
+        <p>Seguici su Instagram!</p>
+    </div>
+    <div class="footerFloat">
+        <ul>
+            <li><a href="https://www.ict.unito.it/">Sito ICT UniTO </a><img src="resources/img/download.png"></li>
+        </ul>
+        
+        <ul>
+            <li><a href="https://www.facebook.com/ICTUNITO/">Facebook ICT UniTO </a><img src="resources/img/download.png"></li>
+        </ul>
+        
+        <ul>
+            <li><a href="chiSiamo.php">Contatti</a></li>
+        </ul>
+    </div>
+</div>
+<div class="underFooter">
+    <p>InnovationTeam © 2019, all rights reserved</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="footer">
+    <a id="footer-desc">Seguici sui nostri canali social</a>
+    <a id="facebook" href=""><i class="fab fa-facebook-f"></i></a>
+    <a id="instagram" href=""><i class="fab fa-instagram"></i></a>
+    <a id="linkedin" href=""><i class="fab fa-linkedin-in"></i></a>
+</div> -->
