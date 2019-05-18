6973c15242643c89a554bd178c0732cc46069132
Luca Aloi <luca.aloi@bitia.it>
2019-05-18 19:06:08 +0200

    aggiugnta pagina index ed header

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+    include_once('partials/header.php');
+?>
+
+<div class="content-body">
+<div class="index-letter">
+
+    <div id="letter-img-left">
+        <img src="resources/img/I.png">
+    </div>
+
+    <div id="let-desc">
+        <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+            literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+            and going through the cites of the word in classical literature, discovered the undoubtable source.
+        </p>
+    </div>
+</div>
+
+<div class="index-letter">
+
+    <div id="letter-img-right">
+        <img src="resources/img/C.png">
+    </div>
+
+    <div id="let-desc">
+        <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+            literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+            and going through the cites of the word in classical literature, discovered the undoubtable source.
+        </p>
+    </div>
+</div>
+
+    <div class="index-letter">
+        <h1></h1>
+    <div id="letter-img-left">
+        <img src="resources/img/T.png">
+    </div>
+
+    <div id="let-desc">
+        <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+            literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+            and going through the cites of the word in classical literature, discovered the undoubtable source.
+        </p>
+    </div>
+</div>
+
+</div>
+
+
+<?php
+    include_once('partials/footer.php')
+?>
---
+++ partials/header.php
@@ -0,0 +1,70 @@
+<?php
+error_reporting(E_ALL); //Imposta quali errori PHP sono segnalati:
+ini_set("display_errors", 1); // Imposta il valore di un'opzione di configurazione ($var,$value)
+session_start();    // Start della sessione
+?>
+
+<!-- <head> del sito web -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="ict, unito, studenti, html, css" />
+    <meta name="description" content="Sito web per studenti di Andrea Carlucci e Rosario Orlando" />
+    <meta name="author" content="Andrea Carlucci, Matteo Ninotti, Flavio Pirazzi, Mahdi, Rachele Veronese, Gabriele Gennari, Edoardo Sina, Luca Aloi" />
+    <!--favicon della pagina che appare accanto al titolo nel browser-->
+
+    <!-- Link al foglio di stile .css-->
+    <link rel="stylesheet" type="text/css" href="resources/css/stile.css" />
+
+    <!-- Set del viewport per il Responsive Design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Link to FontAwesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Innovation Team</title>
+</head>
+<body>
+
+<header>
+
+    <!-- Classe navbar -->
+    <div class="navbar">
+
+        <!-- Logo -->
+        <div id="logo">
+            <a href="index.php">
+                <img src="resources/img/logo.png" alt="Logo">
+            </a>
+        </div>
+        <!-- Fine logo-->
+
+
+        <!--Hamburger da gestire tramite CSS o codice javascript volendo?-->
+        <!--  <a id="myHambuger" href=""><i class="fa fa-bars"</i></a>-->
+
+        <!-- Elementi della nav bar-->
+        <a href="/im-ict/index.php">HOME</a>
+        <a href="/im-ict/news.php">NEWS</a>
+        <a href="/im-ict/tools.php">TOOLS</a>
+        <a href="/im-ict/community.php">COMMUNITY</a>
+        <a href="/im-ict/chiSiamo.php">CHI SIAMO</a>
+
+        <?php
+        if ( isset( $_SESSION['admin'])) {
+            echo'                    
+                    <a href="/im-ict/admin.php">Gestione Utenti</a>
+                    <a href="/im-ict/admin-articoli.php">Gestione Articoli</a>
+                   
+                ';
+        }
+
+        /* Login / Logout e logo utente da fontAwesome */
+        if ( isset( $_SESSION['logged']) ){
+            echo '<a style="float: right" href="/im-ict/logout.php"><i class="fas fa-user"></i> LOG-OUT</a>';
+        } else {
+            echo '<a style="float: right" href="/im-ict/login.php"><i class="fas fa-user"></i> LOG-IN</a>';
+        }
+        ?>
+    </div>
+</header>
