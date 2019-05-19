7fa2e502828538265d7fac1d7212778297d81498
Elisa <so.elisa9@gmail.com>
2019-05-19 17:59:04 +0200

    aggiunti i filtri sulle categorie

--- news.php
+++ news.php
@@ -5,17 +5,14 @@ require 'partials/header.php';
 ?>
 <div class="content-body">
     <div class="searchbar">
-        <form action="/innovazione/news.php" method="POST">
+        <form action="news.php" method="POST">
             <h1>NEWS</h1>
-            <div class="cerca">
-            <input type="text" placeholder="cerca" class="cerca" value="Cerca per titolo o per autore">
-            </div>
             <!-- Categorie -->
             <div class="argomenti">
-            <input type="checkbox" name="categoria_scelta" value="Start_up"> Start up
-            <input type="checkbox" name="categoria_scelta" value="Digital_Marketing"> Digital Marketing
-            <input type="checkbox" name="categoria_scelta" value="New Technogy"> New Technology
-            <input type="checkbox" name="categoria_scelta" value="Web Design"> Web Design
+            <input type="checkbox" name="categorie_scelte[]" value="start up"> Start up
+            <input type="checkbox" name="categorie_scelte[]" value="digital marketing"> Digital Marketing
+            <input type="checkbox" name="categorie_scelte[]" value="new technology"> New Technology
+            <input type="checkbox" name="categorie_scelte[]" value="web design"> Web Design
 
 
 
@@ -28,34 +25,94 @@ require 'partials/header.php';
 
 <?php
 
-$sql = "SELECT * FROM articoli";
-$result = mysqli_query($conn, $sql);
-mysqli_num_rows($result);
-while ($row = mysqli_fetch_assoc($result)) {
 
-    $titolo = $row["titolo"];
-    $sottotitolo = $row["sottotitolo"];
-    $categoria = $row["categoria"];
-    $data = $row["data"];
-    $id = $row["id"];
+if ( isset($_POST['submit-filter']) ) {
 
-    /* Il nome dell'immagine dell'articolo è data dalla stringa 'articolo' + id + '.jpg' */
-    $img = 'articolo'.$id.'.jpg'; // articolo1.jpg
+    $categorie_scelte = $_POST["categorie_scelte"];
 
-    echo '
-                            
-                            
-                            <div style="background-image:url(resources/img/articoli/'.$img.')"; class="articolo">
-                                <form action="/innovation/articolo.php" method="POST">
-                                    <h1><button type="submit" id="submit" name="submit">'. $titolo .'</button></h1>
-                                    <input style="display:none" type="hidden" name="id" value="'.$id.'"></p>
-                                    <h4>' . $sottotitolo . '</h4>
-                                    <h6>Data: ' . $data . '</h6>
-                                </form>
-                            </div>
-                            
-                    ';
+    $stringa_categorie = implode(",", $categorie_scelte);
+
+    $singola_categoria = explode(",", $stringa_categorie);
+
+    /* Se è settata $singola_categoria[0] è uguale al valore, altrimenti uguale a stringa " "! */
+    $categoria1 = isset( $singola_categoria[0]) ? $singola_categoria[0] : ' ';
+    $categoria2 = isset( $singola_categoria[1]) ? $singola_categoria[1] : ' ';
+    $categoria3 = isset( $singola_categoria[2]) ? $singola_categoria[2] : ' ';
+    $categoria4 = isset( $singola_categoria[3]) ? $singola_categoria[3] : ' ';
+
+
+    $sql = "SELECT * FROM articoli WHERE categoria = '" . $categoria1 . "' OR categoria = '" . $categoria2 .
+            "' OR categoria = '" . $categoria3 . "' OR categoria = '" . $categoria4 ."';";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ( mysqli_num_rows($result) > 0 ) {
+
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            $titolo = $row["titolo"];
+            $sottotitolo = $row["sottotitolo"];
+            $categoria = $row["categoria"];
+            $data = $row["data"];
+            $id = $row["id"];
+
+            /* Il nome dell'immagine dell'articolo è data dalla stringa 'articolo' + id + '.jpg' */
+            $img = 'articolo'.$id.'.jpg'; // articolo1.jpg
+
+            echo '
+                       <div style="background-image:url(resources/img/articoli/'.$img.')"; class="articolo">
+                           <form action="/innovation/articolo.php" method="POST">
+                                <h1><button type="submit" id="submit" name="submit">'. $titolo .'</button></h1>
+                                 <input style="display:none" type="hidden" name="id" value="'.$id.'"></p>
+                                 <h4>' . $sottotitolo . '</h4>
+                                 <h6>Data: ' . $data . '</h6>
+                            </form>
+                       </div>
+                                
+                        ';
+            }
+
+        } else {
+            echo 'Non ci sono articoli per la categoria scelta. ';
+        }
+
 }
+else {
+
+    /* Stampo tutti gli articoli presenti nel database */
+    $sql = "SELECT * FROM articoli";
+    $result = mysqli_query($conn, $sql);
+
+    if ( mysqli_num_rows($result) > 0 )
+
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            $titolo = $row["titolo"];
+            $sottotitolo = $row["sottotitolo"];
+            $categoria = $row["categoria"];
+            $data = $row["data"];
+            $id = $row["id"];
+
+            /* Il nome dell'immagine dell'articolo è data dalla stringa 'articolo' + id + '.jpg' */
+            $img = 'articolo' . $id . '.jpg'; // articolo1.jpg
+
+            echo '
+                                
+                                
+                                <div style="background-image:url(resources/img/articoli/' . $img . ')"; class="articolo">
+                                    <form action="/innovation/articolo.php" method="POST">
+                                        <h1><button type="submit" id="submit" name="submit">' . $titolo . '</button></h1>
+                                        <input style="display:none" type="hidden" name="id" value="' . $id . '"></p>
+                                        <h4>' . $sottotitolo . '</h4>
+                                        <h6>Data: ' . $data . '</h6>
+                                    </form>
+                                </div>
+                                
+                        ';
+        }
+}
+
+
 
 ?>
 </div>
