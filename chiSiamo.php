5c1bf60972668e8c8719622369b27917a72a1a83
andrea <andrea.cci95@gmail.com>
2019-05-31 10:26:23 +0200

    inserimento pagina chi siamo

---
+++ chiSiamo.php
@@ -0,0 +1,107 @@
+<?php
+    include_once('partials/header.php')
+?>
+
+<div class="content-body">
+
+<div class="divblock">
+
+    <div class="riga-profile">
+        <div class="titolochisiamo">
+            <h1>Il nostro team</h1>
+        </div>
+    </div>
+
+    <div class="riga-profile">
+        <div class="mezzo-profile">
+            <div class="left-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>Matteo Ninotti</label>
+            </div>
+            <div class="central-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>Elisa Sorri</label>
+            </div>
+        </div>
+        <div class="right-profile">
+            <img src="resources/img/lego.png" height="100" width="100">
+            <label>Rachele Veronese</label>
+        </div>
+    </div>
+
+    <div class="riga-profile">
+        <div class="mezzo-profile">
+            <div class="left-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>Andrea Carlucci</label>
+            </div>
+            <div class="central-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>luca Aloi</label>
+            </div>
+         </div>
+        <div class="right-profile">
+            <img src="resources/img/lego.png" height="100" width="100">
+            <label>Gabriele Gennari</label>
+        </div>
+    </div>
+
+    <div class="riga-profile">
+        <div class="mezzo-profile">
+            <div class="left-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>Mahdi Sarhan</label>
+            </div>
+            <div class="central-profile">
+                <img src="resources/img/lego.png" height="100" width="100">
+                <label>Flavio Pirazzi</label>
+            </div>
+        </div>
+        <div class="right-profile">
+            <img src="resources/img/lego.png" height="100" width="100">
+            <label>Edoardo Sina</label>
+        </div>
+    </div>
+
+</div>
+
+<div class="divblock">
+
+    <div class="riga-profile">
+        <div class="titolochisiamo">
+            <h1>Contatti</h1>
+        </div>
+    </div>
+
+    <div class="riga-profile">
+    <div id="contattacileft">
+        <label>E_mail:</label>
+        <label>Hai bisogno di assistenza o vuoi collaborare con noi?</label>
+        <label>Mandaci una e-mail!</label>
+        <label>CI TROVI ANCHE SU INSTAGRAM</label>
+    </div>
+
+    <div id="contattaciright">
+        <a href="https://www.instagram.com/?hl=it"><img src="resources/img/instagram.png" alt="instagram" height="70" width="80" </a>
+    </div>
+    </div>
+</div>
+
+
+<div class="divblock">
+
+    <div class="riga-profile">
+        <div class="titolochisiamo">
+            <h1>Vieni a trovarci per un caffè!</h1>
+        </div>
+    </div>
+
+    <div id="googlemap" align="center">
+        <iframe title="Campus Luigi Einaudi - UNITO" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2817.582561063833!2d7.6974664149349366!3d45.07397846741813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47887281e1311a15%3A0xd605be48387bc2bd!2sCampus+Luigi+Einaudi!5e0!3m2!1sit!2sit!4v1556825014927!5m2!1sit!2sit" width="600" height="400" frameborder="0" style="border: 0" allowfullscreen></iframe>
+    </div>
+
+</div>
+</div>
+
+<?php
+include_once('partials/footer.php');
