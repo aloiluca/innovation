ef01778717bec0dbe5a89424694e0dd7598faa19
Matteo Ninotti <mattyninotti@gmail.com>
2019-06-07 13:50:32 +0200

    aggiornata funzione replace_special_character()

--- partials/functions.php
+++ partials/functions.php
@@ -9,10 +9,25 @@
  */
 function replace_special_character($text) {
     preg_replace('#à#', '&agrave;', $text);  // Replace à with &agrave;
+    preg_replace('#á#', '&aacute;', $text);  // Replace á with &aacute;
     preg_replace('#è#', '&egrave;', $text);  // Replace è with &egrave;
-    preg_replace('#é#', '&eacute;', $text);  // Replace é with &egrave;
+    preg_replace('#é#', '&eacute;', $text);  // Replace é with &eacute;
     preg_replace('#ì#', '&igrave;', $text);  // Replace ì with &igrave;
+    preg_replace('#í#', '&igrave;', $text);  // Replace í with &iacute;
     preg_replace('#ò#', '&ograve;', $text);  // Replace ò with &ograve;
+    preg_replace('#ó#', '&ograve;', $text);  // Replace ó with &oacute;
+    preg_replace('#ù#', '&ugrave;', $text);  // Replace ù with &ugrave;
+    preg_replace('#ù#', '&ugrave;', $text);  // Replace ú with &uacute;
+    preg_replace('#À#', '&Agrave;', $text);
+    preg_replace('#Á#', '&Aacute;', $text);
+    preg_replace('#È#', '&Egrave;', $text);
+    preg_replace('#É#', '&Eacute;', $text);
+    preg_replace('#Ì#', '&Igrave;', $text);
+    preg_replace('#Í#', '&Iacute;', $text);
+    preg_replace('#Ò#', '&Ograve;', $text);
+    preg_replace('#Ó#', '&Oacute;', $text);
+    preg_replace('#Ù#', '&Ugrave;', $text);
+    preg_replace('#Ú#', '&Uacute;', $text);
     preg_replace('#£#', '&pound;', $text);  // Replace £ with &pound;
     preg_replace('#§#', '&sect;', $text);  // Replace § with &sect;
     preg_replace('#ç#', '&ccedil;', $text);  // Replace ç with &ccedil;
