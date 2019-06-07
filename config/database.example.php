350b372d350a73acb97178c28122dd3a656ff719
Luca Aloi <luca.aloi@bitia.it>
2019-06-07 14:05:23 +0200

    tolto codice inutile

--- config/database.example.php
+++ config/database.example.php
@@ -4,11 +4,7 @@
  * 2. Setta le variabili della tua connessione.
  * */
 
-/* Creazione variabili per connessione al database */
-$servername = "localhost";
-$username = "root";
-$password = "laravel";
-$db = "innovation";
+/* Inserire le variabili per connessione al database */
 
 getConnection('','','','');
 
